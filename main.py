ddab979f344ce8544a923164c560d72e6caf1c88
Ayoub-Bellouch <a.b.bellouch@gmail.com>
2025-07-07 17:19:16 +0100

    Add Config Main .py and other dependencies

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+
+def main():
+    """
+    Entry point of the application.
+    Add your main logic here.
+    """
+    pass  # TODO: implement logic
+
+
+if __name__ == "__main__":
+    main()
