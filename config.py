b50bbff0267109d476273a2cc2f2c5dd458521bf
Ayoub-Bellouch <a.b.bellouch@gmail.com>
2025-07-16 15:55:13 +0100

    add config PLC details

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
+from pylogix import PLC
+import os
+
+
 port     = 5432,
 host     = "RHKEN702",
 database = "P01GES",
 user     = "uwipuser",
 password = "P@ss1m1@n"
+
+
+commSeq= PLC()
+commSeq.IPAddress= "10.212.49.130"
+commSeq.ProcessorSlot = 1
